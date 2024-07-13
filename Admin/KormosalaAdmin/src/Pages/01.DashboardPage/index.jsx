02ad5470af38c0cf0c1f160a6205fb28af9105ef
NicatHuseyn <nicat5075@gmail.com>
2024-07-13 11:31:47 +0400

    add dashboard page

--- Admin/KormosalaAdmin/src/Pages/01.DashboardPage/index.jsx
+++ Admin/KormosalaAdmin/src/Pages/01.DashboardPage/index.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import styles from "./index.module.scss";
+import { NavLink } from "react-router-dom";
+import { endpoints, getAllData } from "../../Services/httpClientServer";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [datas, setDatas] = useState([]);
 
-export default Dashboard
+  useEffect(() => {
+    getAllData(endpoints.jobs).then((res) => {
+      setDatas(res.data);
+    });
+  }, []);
+
+  return (
+    <section>
+      <div className="container">
+        <div className={styles["dashboard-inner"]}>
+          <div className={styles["header"]}>
+            <form>
+              <input type="search" name="" id="" placeholder="Search Job" />
+            </form>
+          </div>
+
+          <div className={styles["jobs"]}>
+            {datas.map((d) => {
+              console.log(d);
+              return (
+                <div className={styles["job-layout"]} key={d.id}>
+                  <div className={styles["layout-inner"]}>
+                    <div className={styles["job-name"]}>
+                      <h3>
+                        <NavLink to={`${endpoints.jobs}/${d.id}`}>
+                          {d.name}
+                        </NavLink>
+                      </h3>
+                    </div>
+
+                    <div className={styles["job-create"]}>
+                      <p>2024-03-04</p>
+                    </div>
+
+                    <div className={styles["job-company"]}>
+                      <h3>Company Name</h3>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Dashboard;
